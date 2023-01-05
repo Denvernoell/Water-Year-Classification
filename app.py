14572cfe3cd4096d28a980f0fa90f6a20255c1c6
Denver Noell <dnoell@ppeng.com>
2023-01-05 06:33:31 -0800

    Error handling

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ st.set_page_config(
 		page_icon="🌊",
 	)
 
+url = 'https://cdec.water.ca.gov/reportapp/javareports?name=wsihist'
 def get_tables():
 	url = 'https://cdec.water.ca.gov/reportapp/javareports?name=wsihist'
 	R = requests.get(url)
@@ -39,38 +40,45 @@ class Location:
 		except Exception as e:
 			st.error(f"Error loading data for {self.name}\n{e}")
 			
-
-			# st.markdown(self.tables[3])
+			st.markdown(self.tables[3])
 			# st.markdown(self.tables[11])
 		
 	def reconstructed(self):
 		table = self.tables[3]
-		st.markdown(table)
-		table = BeautifulSoup(table).find_all('p')[0]
-		rows = [i.split('   ') for i in table.text.split('\r\n')[9:]]
-		df = pd.DataFrame(rows)
-		# display(df)
-		data = df.iloc[:,self.reconstructed_cols]
-		# display(data)
-		data.columns = ["Water Year",'Oct-Mar','Apr-Jul','WYsum','Index','Year type']
-		data = data.astype({'Water Year':'int','Oct-Mar':'float64','Apr-Jul':'float64','WYsum':'float64','Index':'float64','Year type':'string'})
+		try:	
+			# st.markdown(table)
+			table = BeautifulSoup(table).find_all('p')[0]
+			rows = [i.split('   ') for i in table.text.split('\r\n')[9:]]
+			df = pd.DataFrame(rows)
+			# display(df)
+			data = df.iloc[:,self.reconstructed_cols]
+			# display(data)
+			data.columns = ["Water Year",'Oct-Mar','Apr-Jul','WYsum','Index','Year type']
+			data = data.astype({'Water Year':'int','Oct-Mar':'float64','Apr-Jul':'float64','WYsum':'float64','Index':'float64','Year type':'string'})
 
-		data['Year type'] = data['Year type'].pipe(lambda x: x.str.strip().map(abbrs))
+			data['Year type'] = data['Year type'].pipe(lambda x: x.str.strip().map(abbrs))
 
-		self.reconstructed_df = data
+			self.reconstructed_df = data
+		except Exception as e:
+			st.error(f"Error loading data for {self.name}\n{e}")
+			st.markdown(self.tables[3])
 		
 	def forecast(self):
 		table = self.tables[11]
-		table = BeautifulSoup(table).find_all('p')[0]	
-		rows = [i.split('   ') for i in table.text.split('\r\n')[3:]]
-		df = pd.DataFrame(rows)
-		# data = df[[self.reconstructed_cols]]
-		data = df.iloc[:,self.forecast_cols]
-		data.columns = ["Water Year",'Index','Year type']
-		data = data.astype({'Water Year':'int','Index':'float64','Year type':'string'})
-		data['Year type'] = data['Year type'].pipe(lambda x: x.str.strip().map(abbrs))
-		self.forecast_df = data
-		
+		try:
+				
+			table = BeautifulSoup(table).find_all('p')[0]	
+			rows = [i.split('   ') for i in table.text.split('\r\n')[3:]]
+			df = pd.DataFrame(rows)
+			# data = df[[self.reconstructed_cols]]
+			data = df.iloc[:,self.forecast_cols]
+			data.columns = ["Water Year",'Index','Year type']
+			data = data.astype({'Water Year':'int','Index':'float64','Year type':'string'})
+			data['Year type'] = data['Year type'].pipe(lambda x: x.str.strip().map(abbrs))
+			self.forecast_df = data
+		except Exception as e:
+			st.error(f"Error loading data for {self.name}\n{e}")
+			st.markdown(self.tables[11])
 
 
 def plot_location(location):
